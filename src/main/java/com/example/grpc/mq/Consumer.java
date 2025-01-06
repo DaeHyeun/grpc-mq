115455431acf663fd8941b3ccdd54eafb15edb89
Dae Hyun <dh1236@naver.com>
2025-01-06 15:46:06 +0900

    bb

---
+++ src/main/java/com/example/grpc/mq/Consumer.java
@@ -0,0 +1,130 @@
+package com.example.grpc.mq;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+import javax.jms.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
+
+@Service
+@NoArgsConstructor
+public class Consumer implements Runnable, ExceptionListener{
+    private String name;
+    private String receivedId;
+
+    public Consumer(String name, String receivedId) {
+        this.name = name;
+        this.receivedId = receivedId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getReceivedId() {
+        return receivedId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setReceivedId(String receivedId) {
+        this.receivedId = receivedId;
+    }
+
+    public void run() {
+        try {
+            ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+
+            Connection connection = connectionFactory.createConnection();
+            connection.start();
+            connection.setExceptionListener(this);
+
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+            // Destination
+            Destination destination = session.createQueue(name);
+
+            // Create a MessageConsumer from the Session to the Queue
+            MessageConsumer consumer = session.createConsumer(destination);
+
+            // Continuously listen for new messages
+            while (true) {
+                Message message = consumer.receive(); // Wait indefinitely for a new message
+                if (message instanceof TextMessage) {
+                    // Handle text message
+                    TextMessage textMessage = (TextMessage) message;
+                    String text = textMessage.getText();
+                    System.out.println(receivedId + " : " + text);
+                } else if (message instanceof BytesMessage) {
+
+                    // Handle file (BytesMessage)
+                    BytesMessage bytesMessage = (BytesMessage) message;
+                    byte[] fileBytes = new byte[(int) bytesMessage.getBodyLength()];
+                    bytesMessage.readBytes(fileBytes);
+
+                    // Save the file to the specified location
+                    File outputFile = new File("D:\\download", "받은파일" + System.currentTimeMillis() + ".txt");
+                    try (FileOutputStream fos = new FileOutputStream(outputFile)) {
+                        fos.write(fileBytes);
+                            System.out.println("파일전송 및 저장 : " + outputFile.getAbsolutePath());
+                        } catch (IOException e) {
+                            System.out.println("Error saving received file: " + e);
+                        }
+
+                } else if (message instanceof MapMessage) {
+                    // Handle MapMessage
+                    MapMessage mapMessage = (MapMessage) message;
+                    HashMap<String, Object> mapData = new HashMap<>();
+
+                    // Extract data from the MapMessage and store it in the HashMap
+                    for (Enumeration<?> e = mapMessage.getMapNames(); e.hasMoreElements(); ) {
+                        String key = (String) e.nextElement();
+                        Object value = null;
+
+                        // Depending on the type of value, get the corresponding value from the MapMessage
+                        try {
+                            if (mapMessage.itemExists(key)) {
+                                if (mapMessage.getObject(key) instanceof String) {
+                                    value = mapMessage.getString(key);
+                                } else if (mapMessage.getObject(key) instanceof Integer) {
+                                    value = mapMessage.getInt(key);
+                                } else if (mapMessage.getObject(key) instanceof Boolean) {
+                                    value = mapMessage.getBoolean(key);
+                                } else if (mapMessage.getObject(key) instanceof Double) {
+                                    value = mapMessage.getDouble(key);
+                                } else {
+                                    value = mapMessage.getObject(key); // For other types
+                                }
+                                mapData.put(key, value); // Add the key-value pair to the map
+
+                            }
+                        } catch (JMSException e1) {
+                            e1.printStackTrace();
+                        }
+                    }
+                    System.out.println(mapData);
+                } else {
+                    System.out.println(name + " received an unexpected message type.");
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Caught: " + e);
+            e.printStackTrace();
+        }
+    }
+
+    public synchronized void onException(JMSException ex) {
+        System.out.println("JMS Exception occured. Shutting down client.");
+    }
+}
---
+++ src/main/java/com/example/grpc/mq/Procedure.java
@@ -0,0 +1,132 @@
+package com.example.grpc.mq;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.stereotype.Service;
+
+import javax.jms.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@NoArgsConstructor
+public class Procedure implements Runnable{
+    private String name;
+    private String message;
+    private String receivedId;
+    private HashMap<String, Object> mapData;
+    private File file; // To hold the file to be sent
+
+    public Procedure(String name, String message, String receivedId, HashMap<String, Object> mapData, File file) {
+        this.name = name;
+        this.message = message;
+        this.receivedId = receivedId;
+        this.mapData = mapData;
+        this.file = file;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getReceivedId() {
+        return receivedId;
+    }
+
+    public HashMap<String, Object> getMapData() {
+        return mapData;
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setReceivedId(String receivedId) {
+        this.receivedId = receivedId;
+    }
+
+    public void setMapData(HashMap<String, Object> mapData) {
+        this.mapData = mapData;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+
+    @Override
+    public void run() {
+        try {
+
+            // Create a ConnectionFactory
+            ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+
+            // Create a Connection
+            Connection connection = connectionFactory.createConnection();
+            connection.start();
+
+            // Create a Session
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+            // Create the destination
+            Destination destination = session.createQueue(receivedId);
+
+            // Create a MessageProducer from the Session to the Queue
+            MessageProducer producer = session.createProducer(destination);
+            producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+
+            if (file != null) {
+                // If a file is provided, send it as a BytesMessage
+                BytesMessage bytesMessage = session.createBytesMessage();
+                try (FileInputStream fileInputStream = new FileInputStream(file)) {
+                    byte[] fileBytes = new byte[(int) file.length()];
+                    fileInputStream.read(fileBytes);
+                    bytesMessage.writeBytes(fileBytes);
+                }
+                producer.send(bytesMessage); // Send the file as a BytesMessage
+            } else if(mapData != null){
+                // If a message is provided, send a MapMessage
+                MapMessage mapMessage = session.createMapMessage();
+                for (Map.Entry<String, Object> entry : mapData.entrySet()) {
+                    if (entry.getValue() instanceof String) {
+                        mapMessage.setString(entry.getKey(), (String) entry.getValue()); // Add key-value pairs
+                    } else if (entry.getValue() instanceof Integer) {
+                        mapMessage.setInt(entry.getKey(), Integer.parseInt(entry.getKey()));
+                    } else if (entry.getValue() instanceof Character) {
+                        mapMessage.setChar(entry.getKey(), (Character) entry.getValue());
+                    }
+                }
+                // Send the MapMessage
+                producer.send(mapMessage); // Send the MapMessage
+            }else {
+                // If no file, send a regular TextMessage
+                TextMessage textMessage = session.createTextMessage(message);
+                producer.send(textMessage); // Send the text message
+            }
+
+            // Clean up
+            session.close();
+            connection.close();
+        } catch (Exception e) {
+            System.out.println("Caught: " + e);
+            e.printStackTrace();
+        }
+    }
+}
